2a358466c8f94afc6c188b049ede792b3916e8a3
Aaryan Joshi <144917916+Aaryan-Joshi@users.noreply.github.com>
2025-08-01 10:18:32 +0530

    Add files via upload

---
+++ hosteleats/urls.py
@@ -0,0 +1,18 @@
+from django.contrib import admin
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+import os
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('menu.urls')),
+
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=os.path.join(settings.BASE_DIR, "static"))
+
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
